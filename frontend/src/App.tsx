5d53aa035adff84b7d8c6ee1ff230e0c7f360097
TSIDIANY Raharison Muriel <raharisontsidiany@gmail.com>
2024-06-11 21:37:35 +0300

    Exemple ce code avec typescript

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,16 @@
+import { useState } from "react"
 
 function App() {
+  const [prix, setPrix] = useState(1)
 
   return (
-    <>
-     <h1>TYPE SCRIPT</h1>
-    </>
+    <div>
+    <input type="text" value={prix} onChange={(event)=> setPrix(event.target.value)}/>
+     <h1>Mon prix: {prix * 2}</h1>
+    </div>
   )
 }
 
 export default App
+
+//Arret: 28:25
