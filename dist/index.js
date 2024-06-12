aa893ecfbf82beea257bbe45e76d5a73862519d9
TSIDIANY Raharison Muriel <raharisontsidiany@gmail.com>
2024-06-12 12:17:23 +0300

    Interface avec TS

--- dist/index.js
+++ dist/index.js
@@ -1,21 +1,7 @@
 "use strict";
-class FindMember {
-    constructor(nom, position, age) {
-        this.nom = nom;
-        this.position = position;
-        this.age = age;
-    }
-}
-class MemberAdmin extends FindMember {
-    constructor(nom, position, age) {
-        super(nom, position, age);
-        this.creerUnCours = (cours) => `Il (elle) bosse sur ${cours}`;
-    }
-    afficherMemberEnCours(cours) {
-        return `${this.nom} suit le cours de ${cours}`;
-    }
-    ;
-}
-const membre = new MemberAdmin('Murieil', 'Dev', 21);
-console.log(membre.afficherMemberEnCours('Typescript'));
-console.log(membre.creerUnCours('Typescript'));
+const personne = {
+    nom: "Muriel",
+    position: "Dev",
+    age: 15,
+};
+console.log(personne);
--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,15 @@
-abstract class FindMember{
-    constructor(
-        public nom: string, 
-        private position: string, 
-        readonly age: number
-    ){}
+interface Personne {
+    nom: string;
+    position: string;
+    age?: number;
 
-    abstract afficherMemberEnCours(cours: string) :string;
 }
 
-class MemberAdmin extends FindMember{
-    constructor(nom: string, position: string, age: number){
-        super(nom, position, age)
-    }
-    afficherMemberEnCours(cours: string): string {
-        return `${this.nom} suit le cours de ${cours}`
-    };
-
-
-    creerUnCours = (cours: string) => `Il (elle) bosse sur ${cours}`
+const personne: Personne = {
+    nom: "Muriel",
+    position: "Dev",
+    age: 15,
+  
 }
 
-
-
-const membre = new MemberAdmin('Murieil', 'Dev', 21)
-
-console.log(membre.afficherMemberEnCours('Typescript'))
-console.log(membre.creerUnCours('Typescript'))
+console.log(personne)
