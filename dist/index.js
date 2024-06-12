3c4af6c98b33a9a233b53f70128a07e6d6008709
TSIDIANY Raharison Muriel <raharisontsidiany@gmail.com>
2024-06-12 12:44:01 +0300

    Type genericts

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,24 @@
 "use strict";
-let nombresOuChaine = [1, "15", true];
-console.log(nombresOuChaine);
+const utilisateur1 = {
+    nom: 'Muriel',
+    age: 23,
+    competences: {
+        experience: "1 ans",
+        domaine: "Python"
+    }
+};
+const utilisateur2 = {
+    nom: 'Muriel',
+    age: 23,
+    competences: "Typescript"
+};
+const utilisateur3 = {
+    nom: 'Muriel',
+    age: 23,
+    competences: ["Typescript", "Python"]
+};
+const utilisateur4 = {
+    nom: 'Muriel',
+    age: 23,
+    competences: [2]
+};
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,33 @@
-let nombresOuChaine: [number, string, boolean] = [1, "15", true];
+interface Utilisateur<T> {
+    nom: string;
+    age?: number;
+    competences: T
+}
+
+const utilisateur1: Utilisateur<object> = {
+    nom: 'Muriel',
+    age: 23,
+    competences: {
+        experience: "1 ans",
+        domaine: "Python"
+    }
+}
+
+const utilisateur2: Utilisateur<string> = {
+    nom: 'Muriel',
+    age: 23,
+    competences: "Typescript"
+}
+
+const utilisateur3: Utilisateur<string[]> = {
+    nom: 'Muriel',
+    age: 23,
+    competences: ["Typescript", "Python"]
+}
 
 
-console.log(nombresOuChaine)
+const utilisateur4: Utilisateur<Array<number>> = {
+    nom: 'Muriel',
+    age: 23,
+    competences: [2]
+}
