d6edcd97e5a604c6f7218e155142e6336854a16d
TSIDIANY Raharison Muriel <raharisontsidiany@gmail.com>
2024-06-12 12:28:21 +0300

    Les enums

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,15 @@
 "use strict";
+var RoleUtilisateur;
+(function (RoleUtilisateur) {
+    RoleUtilisateur[RoleUtilisateur["ADMIN"] = 0] = "ADMIN";
+    RoleUtilisateur[RoleUtilisateur["EDITOR"] = 1] = "EDITOR";
+    RoleUtilisateur[RoleUtilisateur["USER"] = 2] = "USER";
+    RoleUtilisateur[RoleUtilisateur["VIEWER"] = 3] = "VIEWER";
+})(RoleUtilisateur || (RoleUtilisateur = {}));
 const personne = {
     nom: "Muriel",
-    position: "dev",
+    position: "Developpeur",
     age: 15,
+    role: RoleUtilisateur.ADMIN
 };
 console.log(personne);
--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,25 @@
 type Chaine = string;
-type Position = "dev" | "admin" | "users"
+type Position = "Developpeur" | "IT Support" | "Utilisateur"
 
+enum RoleUtilisateur {
+    ADMIN,
+    EDITOR,
+    USER,
+    VIEWER
+}
 
 type Personne  = {
     nom: Chaine;
     position: Position;
     age?: number;
-
+    role: RoleUtilisateur
 }
-
 
 const personne: Personne = {
     nom: "Muriel",
-    position: "dev",
+    position: "Developpeur",
     age: 15,
-  
+    role: RoleUtilisateur.ADMIN
 }
 
 console.log(personne)
