72f52c5f86ae4c5d429f83fd82d266ed7db55b12
TSIDIANY Raharison Muriel <raharisontsidiany@gmail.com>
2024-06-12 11:59:54 +0300

    Exemple de class

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,18 @@
 "use strict";
-console.log("Salut");
-let message = "Hello World";
-const pi = 3.14;
-console.log(message, pi);
+class FindMember {
+    constructor(nom, position, age) {
+        this.nom = nom;
+        this.position = position;
+        this.age = age;
+        this.afficherMemberEnCours = (cours) => `${this.nom} ${this.position} suit le cours de ${cours}`;
+    }
+}
+class MemberAdmin extends FindMember {
+    constructor(nom, position, age) {
+        super(nom, position, age);
+        this.creerUnCours = (cours) => `Il (elle) bosse sur ${cours}`;
+    }
+}
+const membre = new MemberAdmin('Murieil', 'Dev', 21);
+console.log(membre.afficherMemberEnCours('Typescript'));
+console.log(membre.creerUnCours('Typescript'));
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,23 @@
-console.log("Salut");
-let message = "Hello World";
-const pi = 3.14;
+class FindMember{
+    constructor(
+        public nom: string, 
+        private position: string, 
+        readonly age: number
+    ){}
 
-console.log(message,pi);
+    afficherMemberEnCours = (cours: string) => `${this.nom} ${this.position} suit le cours de ${cours}`;
+}
+
+class MemberAdmin extends FindMember{
+    constructor(nom: string, position: string, age: number){
+        super(nom, position, age)
+    }
+
+    creerUnCours = (cours: string) => `Il (elle) bosse sur ${cours}`
+}
+
+
+const membre = new MemberAdmin('Murieil', 'Dev', 21)
+
+console.log(membre.afficherMemberEnCours('Typescript'))
+console.log(membre.creerUnCours('Typescript'))
